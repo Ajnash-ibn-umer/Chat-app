ccadd82bb0a92fc7da9f0aff988190627b0c4170
ajnash-credot <108794235+ajnash-credot@users.noreply.github.com>
2023-02-09 20:03:54 +0530

    messaginf functionality is completed

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const http = require('http')
 const path = require('path')
 const { Server } = require('socket.io')
 const { fileURLToPath } = require('url')
+const redis =require('redis')
 
+const client=redis.createClient()
 
 const app = express()
 const PORT = 8000
@@ -17,21 +19,23 @@ const io = new Server(server).listen(server)
 app.get('/', (req, res) => {
     res.render('index.ejs')
 })
-io.on('connection',(socket)=>{
-    socket.on('message',({msg,from})=>{
-        console.log(from +' : '+msg);
+io.on('connection', (socket) => {
+    socket.on('message', ({ msg, from }) => {
+        console.log(from + ' : ' + msg);
+        client.rPush('messages',`${from} : ${msg}`)
 
+        io.emit('messageto', { from, msg })
     })
 })
-app.get('/chat',(req,res)=>{
+app.get('/chat', (req, res) => {
     console.log(req.query.username);
-    const username=req.query.username
+    const username = req.query.username
 
-    io.emit('joined',username)
-    io.on('message',(value)=>{
-        console.log({value});
+    io.emit('joined', username)
+    io.on('message', (value) => {
+        console.log({ value });
     })
-    res.render('chat',{username})
+    res.render('chat', { username })
 })
 server.listen(PORT, () => {
     console.log('server at', PORT);
