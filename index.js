2fe21c8fff21357fdd7c76470e88551280d0da66
ajnash-credot <108794235+ajnash-credot@users.noreply.github.com>
2023-02-09 22:01:34 +0530

    project is completed

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const http = require('http')
 const path = require('path')
 const { Server } = require('socket.io')
 const { fileURLToPath } = require('url')
-const redis =require('redis')
+const redis = require('redis')
 
-const client=redis.createClient()
-
+const client = redis.createClient()
+client.connect()
 const app = express()
 const PORT = 8000
 
@@ -15,14 +15,27 @@ app.set('view engine', 'ejs')
 
 const server = http.createServer(app)
 const io = new Server(server).listen(server)
-
+const sendMessage = (async (socket) => {
+    const messages = await client.lRange('messages', '0', '-1')
+    console.log({ messages });
+    messages.map((data) => {
+        const str = data.split(':')
+        const fromUser = str[0]
+        const msgUser = str[1]
+        console.log({fromUser},{msgUser});
+        socket.emit('messageto', { from :fromUser, msg:msgUser })
+    })
+})
 app.get('/', (req, res) => {
     res.render('index.ejs')
 })
-io.on('connection', (socket) => {
-    socket.on('message', ({ msg, from }) => {
+
+io.on('connection', async (socket) => {
+    sendMessage(socket)
+
+    socket.on('message', async ({ msg, from }) => {
         console.log(from + ' : ' + msg);
-        client.rPush('messages',`${from} : ${msg}`)
+        await client.rPush('messages', `${from} : ${msg}`)
 
         io.emit('messageto', { from, msg })
     })
